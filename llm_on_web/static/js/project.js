8d811b9db5c001c97630d9970621fb079db2dcae
Rituparn Shukla <shukla.r@phystech.edu>
2024-06-22 02:04:21 +0530

    Added the about page

--- llm_on_web/static/js/project.js
+++ llm_on_web/static/js/project.js
@@ -1,1 +1,49 @@
-/* Project specific Javascript goes here. */
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+
+const renderer = new THREE.WebGLRenderer({
+
+  alpha: true,
+
+})
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(30, window.innerWidth / window.innerHeight, 5, 2000);
+camera.position.set(-10, 210, 110);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.minDistance = 1;
+controls.maxDistance = 30;
+controls.minPolarAngle = 1;
+controls.maxPolarAngle =0;
+controls.target = new THREE.Vector3(0, 0, 0);
+controls.update();
+
+const ambientLight = new THREE.AmbientLight(0xffffff);
+scene.add(ambientLight);
+const loader = new GLTFLoader().setPath('../static/three-d-models/alpaca/');
+loader.load('scene.gltf', (gltf) => {
+  console.log('loading model');
+  const mesh = gltf.scene;
+  mesh.traverse((child) => {
+    if (child.isMesh) {
+      child.castShadow = true;
+      child.receiveShadow = true;
+    }
+  });
+  mesh.scale.set(6, 6, 6);
+  scene.add(mesh);
+
+});
+
+function animate() {
+  requestAnimationFrame(animate);
+  controls.update();
+  renderer.render(scene, camera);
+}
+animate();
