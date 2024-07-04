c35377cf64305c4bb70fea8b95314a97d89bfc95
Rituparn Shukla <shukla.r@phystech.edu>
2024-07-04 19:48:45 +0530

    The Multimodel also working

--- config/consumers.py
+++ config/consumers.py
@@ -4,8 +4,6 @@ import uuid
 from channels.generic.websocket import AsyncWebsocketConsumer
 from ollama import AsyncClient
 
-import llm.llm_chat as llmchat
-
 
 async def chat(self, usermessage: str):
     message = {"role": "user", "content": f"{usermessage}"}
@@ -27,6 +25,26 @@ async def chat(self, usermessage: str):
         )
 
 
+async def chatllava(self, usermessage: str, image):
+    conv_id = str(uuid.uuid4())
+    async for response in await AsyncClient().generate(
+        "bakllava",
+        usermessage,
+        images=[image],
+        stream=True,
+    ):
+        responsefromllm = response["response"]
+        await self.send(
+            text_data=json.dumps(
+                {
+                    "message": f"{responsefromllm}",
+                    "done": response["done"],
+                    "conv_id": conv_id,
+                },
+            ),
+        )
+
+
 # print(self.scope['user'])  so this reaches every class
 class ChatGuestConsumer(AsyncWebsocketConsumer):
     async def connect(self):
@@ -34,16 +52,7 @@ class ChatGuestConsumer(AsyncWebsocketConsumer):
 
     async def receive(self, text_data=None, bytes_data=None):
         data = json.loads(text_data)
-        if await llmchat.modelcheck() is False:
-            await self.send(
-                text_data=json.dumps(
-                    {
-                        "message": "No LLM models installed, we'll fix it for you",
-                    },
-                ),
-            )
-        else:
-            await chat(self, usermessage=data["message"])
+        await chat(self, usermessage=data["message"])
 
     async def disconnect(self, code):
         await self.close()
@@ -55,39 +64,40 @@ class ChatUserConsumer(AsyncWebsocketConsumer):
 
     async def receive(self, text_data=None, bytes_data=None):
         data = json.loads(text_data)
-        if await llmchat.modelcheck() is False:
-            await self.send(
-                text_data=json.dumps(
-                    {
-                        "message": "No LLM models installed, we'll fix it for you",
-                    },
-                ),
-            )
-        else:
-            await chat(self, usermessage=data["message"])
+        await chat(self, usermessage=data["message"])
 
     async def disconnect(self, code):
         await self.close()
 
 
+# Todo: Getting the correct data from the server
 class ChatLlavaGConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         await self.accept()
 
     async def receive(self, text_data=None, bytes_data=None):
-        pass
+        data = json.loads(text_data)
+        usermessage = data["message"]
+        imagedata = data["imgDataToServer"]
+        await chatllava(self, usermessage=usermessage, image=imagedata)
 
     async def disconnect(self, code):
         await self.close()
 
 
+#  TODO: USE THE USER TEXT TO PROMPT AND THEN SEND THE RESPONSE BACK
 class ChatLlavaUConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         await self.accept()
-        await self.send(text_data="Connection Made")
 
     async def receive(self, text_data=None, bytes_data=None):
-        pass
+        await self.send(
+            text_data=json.dumps(
+                {
+                    "message": f"{text_data}",
+                },
+            ),
+        )
 
     async def disconnect(self, code):
         await self.close()
--- llm_on_web/static/js/chat-handle.js
+++ llm_on_web/static/js/chat-handle.js
@@ -11,7 +11,6 @@ const chatResponse=document.getElementById("chat-response")
     form.addEventListener('submit', (e)=>{
         e.preventDefault();
         const message= document.getElementById('user-query-text').value;
-
         const id=document.getElementById('who').value.trim();
         console.log(message);
         console.log(id);
@@ -45,7 +44,7 @@ const chatResponse=document.getElementById("chat-response")
                 llmresponseContainer.classList.add('flex', 'justify-start');
                 llm_container = document.createElement('div');
                 llm_container.id = `llm-response_${conv_id}`;
-                llm_container.classList.add('bg-opacity-30', 'mt-2', 'mb-2', 'bg-gray-800', 'text-white', 'font-sans1', 'rounded-lg', 'py-2', 'px-4', 'max-w-sm', 'outline', 'outline-offset-2', 'outline-cyan-500');
+                llm_container.classList.add('bg-opacity-30', 'mt-2', 'mb-2', 'bg-gray-800', 'text-white', 'break-words','font-sans1', 'rounded-lg', 'py-2', 'px-4', 'max-w-sm', 'outline', 'outline-offset-2', 'outline-cyan-500');
                 llm_container.textContent = messagefromserver.message;
                 llmresponseContainer.appendChild(llm_container);
                 chatResponse.appendChild(llmresponseContainer);
@@ -66,7 +65,7 @@ const chatResponse=document.getElementById("chat-response")
                 llmresponseContainer.classList.add('flex', 'justify-start');
                 llm_container = document.createElement('div');
                 llm_container.id = `llm-response_${conv_id}`;
-                llm_container.classList.add('bg-opacity-30', 'mt-2', 'mb-2', 'bg-gray-800', 'text-white', 'font-sans1', 'rounded-lg', 'py-2', 'px-4', 'max-w-sm', 'max-h-64', 'overflow-y-auto', 'outline', 'outline-offset-2', 'outline-cyan-500');
+                llm_container.classList.add('bg-opacity-30', 'mt-2', 'mb-2', 'bg-gray-800', 'text-white', 'font-sans1', 'rounded-lg', 'py-2', 'px-4', 'max-w-sm', 'break-words', 'overflow-y-auto', 'outline', 'outline-offset-2', 'outline-cyan-500');
                 llm_container.textContent = messagefromserver.message;
                 llmresponseContainer.appendChild(llm_container);
                 chatResponse.appendChild(llmresponseContainer);
